f86fcee6f34af701aa18b2af9c38d5f9e39ed417
Yukimasa Sugizaki <ysugi@idein.jp>
2021-10-11 19:47:18 +0900

    fixup

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM scratch
-ADD root.tar /
+ARG TARGETPLATFORM
+ADD root-$TARGETPLATFORM.tar /
 CMD ["/bin/bash"]
--- generate-root.bash
+++ generate-root.bash
@@ -2,38 +2,54 @@
 
 set -e -u -x -o pipefail
 
-URL="$1"
-BASENAME="${URL##*/}"
-BASENAME="${BASENAME%.zip}"
+URL_ARMHF="$1"
+URL_ARM64="$2"
 
-curl -LO "$URL"
+for URL in "$URL_ARMHF" "$URL_ARM64"; do
 
-if curl -LO "$URL.sig"; then
-	curl -LO 'https://www.raspberrypi.org/raspberrypi_downloads.gpg.key'
-	gpg --import 'raspberrypi_downloads.gpg.key'
-	gpg --verify "$BASENAME.zip.sig" "$BASENAME.zip"
-fi
+	if [ -z "$URL" ]; then
+		continue
+	fi
 
-if curl -LO "$URL.sha1"; then
-	sha1sum -c "$BASENAME.zip.sha1"
-fi
+	if [ "$URL" = "$URL_ARMHF" ]; then
+		ARCH='armhf'
+	else
+		ARCH='arm64'
+	fi
 
-if curl -LO "$URL.sha256"; then
-	sha256sum -c "$BASENAME.zip.sha256"
-fi
+	BASENAME="${URL##*/}"
+	BASENAME="${BASENAME%.zip}"
 
-unzip "$BASENAME.zip"
+	curl -LO "$URL"
 
-partx -bs "$BASENAME.img"
-PART1_START=$(partx -bgr -o START -n 1 "$BASENAME.img")
-PART2_START=$(partx -bgr -o START -n 2 "$BASENAME.img")
-PART1_SIZE=$(partx -bgr -o SIZE -n 1 "$BASENAME.img")
-PART2_SIZE=$(partx -bgr -o SIZE -n 2 "$BASENAME.img")
+	if curl -LO "$URL.sig"; then
+		curl -LO 'https://www.raspberrypi.org/raspberrypi_downloads.gpg.key'
+		gpg --import 'raspberrypi_downloads.gpg.key'
+		gpg --verify "$BASENAME.zip.sig" "$BASENAME.zip"
+	fi
 
-mkdir mnt/
-sudo mount -o "ro,loop,offset=$((PART2_START * 512)),sizelimit=$PART2_SIZE" -t ext4 "$BASENAME.img" mnt/
-sudo mount -o "ro,loop,offset=$((PART1_START * 512)),sizelimit=$PART1_SIZE" -t vfat "$BASENAME.img" mnt/boot/
+	if curl -LO "$URL.sha1"; then
+		sha1sum -c "$BASENAME.zip.sha1"
+	fi
 
-sudo tar Ccf mnt/ - . > root.tar
+	if curl -LO "$URL.sha256"; then
+		sha256sum -c "$BASENAME.zip.sha256"
+	fi
 
-sudo umount mnt/boot/ mnt/
+	unzip "$BASENAME.zip"
+
+	partx -bs "$BASENAME.img"
+	PART1_START=$(partx -bgr -o START -n 1 "$BASENAME.img")
+	PART2_START=$(partx -bgr -o START -n 2 "$BASENAME.img")
+	PART1_SIZE=$(partx -bgr -o SIZE -n 1 "$BASENAME.img")
+	PART2_SIZE=$(partx -bgr -o SIZE -n 2 "$BASENAME.img")
+
+	mkdir mnt/
+	sudo mount -o "ro,loop,offset=$((PART2_START * 512)),sizelimit=$PART2_SIZE" -t ext4 "$BASENAME.img" mnt/
+	sudo mount -o "ro,loop,offset=$((PART1_START * 512)),sizelimit=$PART1_SIZE" -t vfat "$BASENAME.img" mnt/boot/
+
+	sudo tar Ccf mnt/ - . > "root-$ARCH.tar"
+
+	sudo umount mnt/boot/ mnt/
+
+done
