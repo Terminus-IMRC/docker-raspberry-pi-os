c6ea2d6d53c5a8de38231da6f7977310438df8a5
Yukimasa Sugizaki <ysugi@idein.jp>
2021-10-11 20:43:26 +0900

    fixup

--- generate-root.bash
+++ generate-root.bash
@@ -2,57 +2,52 @@
 
 set -e -u -x -o pipefail
 
-URL_ARMHF="$1"
-URL_ARM64="$2"
+IMAGE="$1"
+URL="$2"
 
-for URL in "$URL_ARMHF" "$URL_ARM64"; do
+if [[ $IMAGE = *-armhf ]]; then
+	PLATFORM='linux/arm32/v6'
+else
+	PLATFORM='linux/arm64/v8'
+fi
 
-	if [ -z "$URL" ]; then
-		continue
-	fi
+BASENAME="${URL##*/}"
+BASENAME="${BASENAME%.zip}"
 
-	if [ "$URL" = "$URL_ARMHF" ]; then
-		ARCH='armhf'
-	else
-		ARCH='arm64'
-	fi
+curl -LO "$URL"
 
-	BASENAME="${URL##*/}"
-	BASENAME="${BASENAME%.zip}"
+if curl -LO "$URL.sig"; then
+	curl -LO 'https://www.raspberrypi.org/raspberrypi_downloads.gpg.key'
+	gpg --import 'raspberrypi_downloads.gpg.key'
+	gpg --verify "$BASENAME.zip.sig" "$BASENAME.zip"
+	rm "$BASENAME.zip.sig"
+fi
 
-	curl -LO "$URL"
+if curl -LO "$URL.sha1"; then
+	sha1sum -c "$BASENAME.zip.sha1"
+	rm "$BASENAME.zip.sha1"
+fi
 
-	if curl -LO "$URL.sig"; then
-		curl -LO 'https://www.raspberrypi.org/raspberrypi_downloads.gpg.key'
-		gpg --import 'raspberrypi_downloads.gpg.key'
-		gpg --verify "$BASENAME.zip.sig" "$BASENAME.zip"
-	fi
+if curl -LO "$URL.sha256"; then
+	sha256sum -c "$BASENAME.zip.sha256"
+	rm "$BASENAME.zip.sha256"
+fi
 
-	if curl -LO "$URL.sha1"; then
-		sha1sum -c "$BASENAME.zip.sha1"
-	fi
+unzip "$BASENAME.zip"
+rm "$BASENAME.zip"
 
-	if curl -LO "$URL.sha256"; then
-		sha256sum -c "$BASENAME.zip.sha256"
-	fi
+partx -bs "$BASENAME.img"
+PART1_START=$(partx -bgr -o START -n 1 "$BASENAME.img")
+PART2_START=$(partx -bgr -o START -n 2 "$BASENAME.img")
+PART1_SIZE=$(partx -bgr -o SIZE -n 1 "$BASENAME.img")
+PART2_SIZE=$(partx -bgr -o SIZE -n 2 "$BASENAME.img")
 
-	unzip "$BASENAME.zip"
-	rm "$BASENAME.zip"
+mkdir mnt/
+sudo mount -o "ro,loop,offset=$((PART2_START * 512)),sizelimit=$PART2_SIZE" -t ext4 "$BASENAME.img" mnt/
+sudo mount -o "ro,loop,offset=$((PART1_START * 512)),sizelimit=$PART1_SIZE" -t vfat "$BASENAME.img" mnt/boot/
 
-	partx -bs "$BASENAME.img"
-	PART1_START=$(partx -bgr -o START -n 1 "$BASENAME.img")
-	PART2_START=$(partx -bgr -o START -n 2 "$BASENAME.img")
-	PART1_SIZE=$(partx -bgr -o SIZE -n 1 "$BASENAME.img")
-	PART2_SIZE=$(partx -bgr -o SIZE -n 2 "$BASENAME.img")
+sudo tar Ccf mnt/ - . | docker import --change 'CMD ["/bin/bash"]' --platform "$PLATFORM" - "$IMAGE"
 
-	mkdir mnt/
-	sudo mount -o "ro,loop,offset=$((PART2_START * 512)),sizelimit=$PART2_SIZE" -t ext4 "$BASENAME.img" mnt/
-	sudo mount -o "ro,loop,offset=$((PART1_START * 512)),sizelimit=$PART1_SIZE" -t vfat "$BASENAME.img" mnt/boot/
-
-	sudo tar Ccf mnt/ - . > "root-$ARCH.tar"
-
-	sudo umount mnt/boot/ mnt/
-	rmdir mnt/
-	rm "$BASENAME.img"
-
-done
+sudo umount mnt/boot/ mnt/
+rmdir mnt/
+rm "$BASENAME.img"
